e3ad27a9c07694f1a00f885faf9686748172c6d4
Will Gu <willgu29@gmail.com>
2018-07-27 20:35:56 -0700

    removed loader

--- index.js
+++ index.js
@@ -18,36 +18,36 @@ module.exports = function nuxtMarkdownit (options) {
       // Vue template support
       const vueLoader = config.module.rules.find(rule => rule.test.toString() === '/\\.vue$/')
       // Checks for query loaders first (<= rc5)
-      // if (vueLoader.query && vueLoader.query.loaders) {
-      //   vueLoader.query.loaders['md'] = markDownItLoader
-      // } else {
-      //    // Sets options loaders (>= rc6)
-      //   vueLoader.options.loaders['md'] = markDownItLoader
-      // }
+      if (vueLoader.query && vueLoader.query.loaders) {
+        vueLoader.query.loaders['md'] = markDownItLoader
+      } else {
+         // Sets options loaders (>= rc6)
+        vueLoader.options.loaders['md'] = markDownItLoader
+      }
       // .md Loader
-      config.module.rules.push({
-        test: /\.md$/,
-        use: [
-          'raw-loader'
-          // markDownItLoader
-        ]
-      })
+      // config.module.rules.push({
+      //   test: /\.md$/,
+      //   use: [
+      //     'raw-loader'
+      //     // markDownItLoader
+      //   ]
+      // })
     } else {
-      config.module.rules.push({
-        test: /\.md$/,
-        oneOf: [
-          // {
-          //   resourceQuery: /^\?vue/,
-          //   use: [markDownItLoader]
-          // },
-          {
-            use: [
-              'raw-loader'
-              // markDownItLoader
-            ]
-          }
-        ]
-      })
+      // config.module.rules.push({
+      //   test: /\.md$/,
+      //   oneOf: [
+      //     {
+      //       resourceQuery: /^\?vue/,
+      //       use: [markDownItLoader]
+      //     },
+      //     {
+      //       use: [
+      //         'raw-loader'
+      //         // markDownItLoader
+      //       ]
+      //     }
+      //   ]
+      // })
     }
   })
 
